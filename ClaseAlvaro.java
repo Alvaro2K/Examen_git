4c03b09e238ce874edf3ff7de2d649b3c5abf753
Alvaro2K <agustin.gaalv22@zaragoza.salesianos.edu>
2023-05-09 17:47:07 +0200

    Alvaro 17:47

--- ClaseAlvaro.java
+++ ClaseAlvaro.java
@@ -11,7 +11,7 @@ public class ClaseAlvaro
     // clase donde se escribe el nombre
     public void escribirnombre()
     {
-        System.out.println("Mi nombre es:          " + nombre);
+        System.out.println("Mi nombre es:       " + nombre);
     }
 
 }
